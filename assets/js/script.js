2e41f971b08353ba7a16b51cc7661ef3f5151d4c
gitkcb <kcharlesbell@gmail.com>
2022-10-18 12:23:41 -0400

    organized schedule times and add text boxes to schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
 
 let time = moment();
 $('#currentDay').text(time.format('MMMM Do YYYY, h:mm:ss a'));
+
+/*var scheduleHourNine = $('#hour9');
+var buttonNine = $('#btnhour9');
+function saveHourNine(){
+    let scheduleHourNine = scheduleHourNine.value.trim();
+    localStorage.setItem("scheduleHourNine", JSON.stringify(saveHourNine));
+}
+function renderHourNine(){
+    var onScheduleNine = JSON.parse(localStorage.getItem('scheduleHourNine'));
+    if(onScheduleNine!== null){
+        scheduleHourNine.innerHTML = onScheduleNine;
+    }else{
+        return;
+    }
+}
+function clickButtonNine(){
+    preventDefault();
+    saveHourNine();
+    renderHourNine();
+}
+buttonNine.onclick= clickButtonNine;
+function init(){
+    renderHourNine();
+}
+init();*/
+
+
+
+
+
