3aba492fa0b4582c4c866d787874ad025919eb91
gitkcb <kcharlesbell@gmail.com>
2022-10-18 14:44:28 -0400

    added function that changes time blocks to past present and future colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
 
-let time = moment();
-$('#currentDay').text(time.format('MMMM Do YYYY, h:mm:ss a'));
+let date = moment();
+$('#currentDay').text(date.format('MMMM Do YYYY, h:mm:ss a'));
+let timeBlocks = $(".time-block");
+let hour = moment().format('H')
+console.log(hour); 
+function timeBlockColor(){
+    timeBlocks.each(function(){
+        let thisBlock = $(this);
+        let thisBlockHour = parseInt(thisBlock.attr('data-hour'));
+    
+    if(thisBlockHour == hour){
+        thisBlock.addClass('present').removeClass('past future');
+    }
+    else if(thisBlockHour < hour){
+        thisBlock.addClass('past').removeClass('present future');
+
+    }else if(thisBlockHour > hour){
+        thisBlock.addClass('future').removeClass('past present');
+    }
+});
+
+}
+timeBlockColor();
 
 /*var scheduleHourNine = $('#hour9');
 var buttonNine = $('#btnhour9');
